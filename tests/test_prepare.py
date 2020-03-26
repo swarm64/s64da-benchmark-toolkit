396e6c9c22976a0d4223458e6602a9ab70567eab
Sebastian Dressler <dressler@mailbox.org>
2020-03-26 11:00:30 +0100

    Add disk size check before ingest

---
+++ tests/test_prepare.py
@@ -0,0 +1,50 @@
+import pytest
+
+from s64da_benchmark_toolkit.prepare import PrepareBenchmarkFactory
+from s64da_benchmark_toolkit.streams import Benchmark
+
+
+@pytest.fixture()
+def prepare_mock(mocker):
+    class Args:
+        schema = 'sdb_hooray'
+        scale_factor = 100
+
+    class PrepareTestBenchmark(PrepareBenchmarkFactory):
+        PrepareBenchmarkFactory.SIZING_FACTORS = {
+            's64da': {
+                100: 0.5
+            }
+        }
+
+    mocker.patch('os.path.isdir', return_value=True)
+    return PrepareTestBenchmark(Args(),Benchmark('foobar', '/bla/bleh/foobar'))
+
+
+def test_check_diskspace_ok(mocker, prepare_mock):
+    mocker.patch('shutil.disk_usage',
+                 return_value=(150 * 1024 * 1024, 50 * 1024 * 1024, 100 * 1024 * 1024),
+                 autospec=True)
+
+    # Must not raise an AssertionError
+    prepare_mock._check_diskspace('/xyz')
+
+
+def test_check_diskspace_not_ok(mocker, prepare_mock):
+    mocker.patch('shutil.disk_usage',
+                 return_value=(150 * 1024 * 1024, 100 * 1024 * 1024, 50 * 1024 * 1024),
+                 autospec=True)
+
+    with pytest.raises(AssertionError):
+        prepare_mock._check_diskspace('/xyz')
+
+
+def test_check_diskspace_scale_factor_off(mocker, prepare_mock):
+    mocker.patch('shutil.disk_usage',
+                 return_value=(150 * 1024 * 1024, 100 * 1024 * 1024, 50 * 1024 * 1024),
+                 autospec=True)
+
+    prepare_mock.args.scale_factor = 123
+
+    # Must not raise an AssertionError
+    prepare_mock._check_diskspace('/xyz')
