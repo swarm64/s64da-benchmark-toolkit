8e4f7b4146e442adfd2544a15b5492d122743ab4
David Geier <david@swarm64.com>
2020-04-09 09:18:04 +0200

    Create extension in s64da_native_tables_no_indexes

--- benchmarks/tpcds/schemas/s64da_native_tables_no_indexes/schema.sql
+++ benchmarks/tpcds/schemas/s64da_native_tables_no_indexes/schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION swarm64da;
+
 -- round(double precision, int) does not exist by default
 CREATE FUNCTION round(double precision, integer) RETURNS NUMERIC AS $$
     SELECT round($1::numeric, $2)
