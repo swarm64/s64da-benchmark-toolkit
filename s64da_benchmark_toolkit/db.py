ebb9c14bfe7b0572ef05c86f754538cb43430f91
Ervin Madaha <vicervin95@gmail.com>
2020-05-29 16:14:25 +0200

    Changed JSON Decode Error to a LOG warning

--- s64da_benchmark_toolkit/db.py
+++ s64da_benchmark_toolkit/db.py
@@ -101,5 +101,5 @@ class DB:
             return f'{{"Explain Output failed": "{str(e)}"}}'
 
         except json.JSONDecodeError as e:
-            LOG.error('Explain Output failed with a JSON Decode Error')
-            return f'{{"Explain Output failed with a JSON Decode Error": "{str(e)}"}}'
+            LOG.warning('Explain Output failed with a JSON Decode Error')
+            return f'Explain Output failed with a JSON Decode Error: {str(e)}'
