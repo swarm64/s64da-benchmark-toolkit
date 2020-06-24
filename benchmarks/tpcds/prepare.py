89ae0b3b369a93f5b92691dccfc152933f90f0c3
Ervin Madaha <vicervin95@gmail.com>
2020-06-24 11:05:04 +0200

    filtered the generator output to avoid leak to stdout

--- benchmarks/tpcds/prepare.py
+++ benchmarks/tpcds/prepare.py
@@ -55,7 +55,8 @@ class PrepareBenchmark(PrepareBenchmarkFactory):
         if '_sales' in table:
             returns_table = table.split('_')[0] + '_returns'
             task += (f'tee >({self._stream_to_db(table)}) '
-                     f'>({self._stream_to_db(returns_table)})')
+                     f'>({self._stream_to_db(returns_table)}) | '
+                     f'grep -vE "^({table}|{returns_table})"') # Hide the dsdgen output from the loader output
         else:
             task += f'{self._stream_to_db(table)}'
 
