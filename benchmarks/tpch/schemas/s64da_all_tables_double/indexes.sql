cde0b993a356133b94bf0f44200903abe9cbd063
Juri Schmidt <juri.schmidt@swarm64.com>
2020-08-13 17:17:38 +0200

    Remove deprecated all_tables schemas

--- benchmarks/tpch/schemas/s64da_all_tables_double/indexes.sql
+++
@@ -1,1 +0,0 @@
-CREATE STATISTICS lineitem_ndistinct (ndistinct) ON l_suppkey, l_partkey FROM lineitem; 
--- benchmarks/tpch/schemas/s64da_all_tables_double/primary-keys.sql
+++
@@ -1,8 +0,0 @@
-ALTER FOREIGN TABLE customer OPTIONS(ADD primary_key_hint 'PRIMARY KEY (c_custkey)');
-ALTER FOREIGN TABLE nation OPTIONS(ADD primary_key_hint 'PRIMARY KEY (n_nationkey)');
-ALTER FOREIGN TABLE part OPTIONS(ADD primary_key_hint 'PRIMARY KEY (p_partkey)');
-ALTER FOREIGN TABLE partsupp OPTIONS(ADD primary_key_hint 'PRIMARY KEY (ps_partkey, ps_suppkey)');
-ALTER FOREIGN TABLE region OPTIONS(ADD primary_key_hint 'PRIMARY KEY (r_regionkey)');
-ALTER FOREIGN TABLE supplier OPTIONS(ADD primary_key_hint 'PRIMARY KEY (s_suppkey)');
-ALTER FOREIGN TABLE lineitem OPTIONS(ADD primary_key_hint 'PRIMARY KEY (l_linenumber, l_orderkey)');
-ALTER FOREIGN TABLE orders OPTIONS(ADD primary_key_hint 'PRIMARY KEY (o_orderkey)');
