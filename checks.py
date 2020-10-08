2808b81227940b2e3826ed3939aa35dc39611c69
Spiros N. Agathos <spiros@swarm64.com>
2020-10-08 13:50:27 +0200

    Removed ./loader.sh and and functions.sh scripts Modified documantion to include instructions for testing EPAS
    For PSQL the default user is postgres
    For EPAS the default user is enterprisedb
    
    P.S.
    Both PSQL and EPAS Swarm product images include by default a database named
    postgres.

---
+++ checks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from shutil import which
+import sys
+
+
+def check_program_exists(program):
+    if which(program) is None:
+        print('ERROR: Could not find {}. Is it installed? Is PATH setup properly?'.format(program))
+        sys.exit()
