65a43daf93eeb915f0f075d07d47e2248f12f73b
Sebastian Dreßler <sebastian@swarm64.com>
2020-10-12 16:46:08 +0200

    Create a separate lib module for TPC-C

---
+++ benchmarks/tpcc/lib/output.py
@@ -0,0 +1,165 @@
+
+import sys
+import time
+
+from datetime import datetime
+from uuid import uuid4
+
+from psycopg2.extras import execute_values, register_uuid
+from reprint import output
+
+from .queries import QUERIES
+from s64da_benchmark_toolkit.dbconn import DBConn
+
+register_uuid()
+
+QUERIES_LIST = sorted(QUERIES.keys())
+QUERIES_OUTPUT_MAP = {QUERIES_LIST[idx]: idx for idx in range(len(QUERIES_LIST))}
+
+
+class TxView:
+    def __init__(self):
+        self.counter = 0
+        self.metrics = {
+            'ok': {
+                'min': sys.maxsize,
+                'max': 0,
+                'avg': 0,
+                'current': 0,
+                'total': 0
+            },
+            'err': {
+                'min': sys.maxsize,
+                'max': 0,
+                'avg': 0,
+                'current': 0,
+                'total': 0
+            },
+            'total': {
+                'min': sys.maxsize,
+                'max': 0,
+                'avg': 0,
+                'current': 0,
+                'total': 0
+            }
+        }
+
+    def add_tx_info(self, ok, err):
+        self.metrics['ok']['current'] = ok
+        self.metrics['ok']['total'] += ok
+
+        self.metrics['err']['current'] = err
+        self.metrics['err']['total'] += err
+
+        self.metrics['total']['current'] = (ok + err)
+        self.metrics['total']['total'] += (ok + err)
+
+        self.counter += 1
+
+        for key in ('ok', 'err', 'total'):
+            entry = self.metrics[key]
+            entry['avg'] = entry['total'] / self.counter
+            entry['min'] = min(entry['min'], entry['current'])
+            entry['max'] = max(entry['max'], entry['current'])
+
+    @property
+    def output(self):
+        output = []
+        for key in ('ok', 'err', 'total'):
+            entry = self.metrics[key]
+            s  = f"Tx {key:>6} - "
+            s += f"Now: {entry['current']:>5.0f} tps  "
+            s += f"AVG: {entry['avg']:>5.0f} tps  "
+            s += f"MIN: {entry['min']:>5.0f} tps  "
+            s += f"MAX: {entry['max']:>5.0f} tps  "
+            s += f"TOTAL: {entry['total']: >10,.0f}"
+            output.append(s)
+        return output
+
+class QueryRuntimeView:
+    class Buffer:
+        def __init__(self, capacity):
+            self.counter = 0
+            self._data = [None] * capacity
+
+        def push(self, datum):
+            self.counter += 1
+            self._data.insert(0, datum)
+            self._data.pop()
+
+        @property
+        def all(self):
+            return (self.counter, self._data)
+
+    def __init__(self, query_num):
+        self.query_num = query_num
+        self.status = '-'
+        self.runtimes = QueryRuntimeView.Buffer(3)
+
+    def add_runtime(self, runtime):
+        self.runtimes.push(round(runtime, 3))
+
+    def set_status(self, status):
+        self.status = status
+
+    def __str__(self):
+        counter, runtimes = self.runtimes.all
+        runtimes = ' | '.join([f'{x:>10.3f}' for x in runtimes if x])
+        return f'Q{self.query_num:<3}: {counter:>3}x - {self.status:<8} - {runtimes}'
+
+
+class Output:
+    def __init__(self, dsn):
+        self.uuid = uuid4()
+        self.dsn = dsn
+        self.tx_view = TxView()
+        self.query_runtimes = []
+        for query in QUERIES_LIST:
+            self.query_runtimes.append(QueryRuntimeView(query))
+
+    def display(self, lock, counter_ok, counter_err, order_timestamp,
+                query_queue, end_timestamp, oltp_workers):
+        with output(output_type="list", initial_len=20, interval=0) as output_list:
+            output_list[0] = self.uuid
+            with DBConn(self.dsn) as dbconn:
+                for idx in range(1,20):
+                    output_list[idx] = ' '
+
+                while True:
+                    counter_ok_tmp = 0
+                    counter_err_tmp = 0
+                    time.sleep(1)
+                    with lock:
+                        counter_ok_tmp = counter_ok.value
+                        counter_err_tmp = counter_err.value
+                        counter_ok.value = 0
+                        counter_err.value = 0
+
+                    current_timestamp = datetime.now()
+                    timestamp = datetime.fromtimestamp(order_timestamp.value)
+                    self.tx_view.add_tx_info(counter_ok_tmp, counter_err_tmp)
+
+                    dbconn.cursor.execute(
+                        'INSERT INTO oltp_stats VALUES(%s, %s, %s, %s)',
+                        (self.uuid, current_timestamp, counter_ok_tmp, counter_err_tmp))
+
+                    db_buffer = []
+                    while not query_queue.empty():
+                        worker_id, query, runtime = query_queue.get_nowait()
+                        output_idx = QUERIES_OUTPUT_MAP.get(query, None)
+                        if output_idx is not None and isinstance(runtime, float):
+                            self.query_runtimes[output_idx].add_runtime(runtime)
+                            self.query_runtimes[output_idx].set_status('done')
+                            db_buffer.append((self.uuid, current_timestamp, worker_id, query, runtime))
+
+                    if db_buffer:
+                        sql = 'INSERT INTO olap_stats VALUES %s'
+                        execute_values(dbconn.cursor, sql, db_buffer)
+
+                    output_list[1] = f'Current timestamp: {timestamp}'
+                    output_list[2], output_list[3], output_list[4] = self.tx_view.output
+                    for idx, entry in enumerate(self.query_runtimes):
+                        output_list[idx + 6] = str(entry)
+
+                    if oltp_workers > 0 and timestamp > end_timestamp:
+                        return True
--- benchmarks/tpcc/run.py
+++ benchmarks/tpcc/run.py
@@ -2,125 +2,16 @@
 
 import argparse
 import sys
+import traceback
 
 from datetime import datetime, timedelta
 from multiprocessing.pool import Pool
 from multiprocessing import Value, Lock, Queue
 
-import psycopg2
-
-from benchmarks.tpcc.helpers import Random
-from benchmarks.tpcc.queries import Queries
-from benchmarks.tpcc.output import Output
+from benchmarks.tpcc.lib.queries import Queries
+from benchmarks.tpcc.lib.output import Output
+from benchmarks.tpcc.lib.tpcc import TPCC
 from s64da_benchmark_toolkit.dbconn import DBConn
-
-MAXITEMS = 100000
-DIST_PER_WARE = 10
-CUST_PER_DIST = 3000
-NUM_ORDERS = 3000
-STOCKS = 100000
-NAMES = ['BAR', 'OUGHT', 'ABLE', 'PRI', 'PRES', 'ESE', 'ANTI', 'CALLY', 'ATION', 'EING']
-
-
-class TPCC:
-    def __init__(self, seed, scale_factor):
-        self.random = Random(seed)
-        self.scale_factor = scale_factor
-
-    def other_ware(self, home_ware):
-        if self.scale_factor == 1:
-            return home_ware
-
-        while True:
-            tmp = self.random.randint_inclusive(1, self.scale_factor)
-            if tmp == home_ware:
-                return tmp
-
-    @classmethod
-    def execute_sql(cls, conn, sql, args):
-        try:
-            conn.cursor.execute(sql, args)
-            result = conn.cursor.fetchall()
-            # assert len(result) == order_line_count
-        except psycopg2.Error:
-            return False
-
-        return True
-
-    def new_order(self, conn, timestamp='NOW()'):
-        w_id = self.random.randint_inclusive(1, self.scale_factor)
-        d_id = self.random.randint_inclusive(1, DIST_PER_WARE)
-        c_id = self.random.nurand(1023, 1, CUST_PER_DIST)
-        order_line_count = self.random.randint_inclusive(5, 15)
-        rbk = self.random.randint_inclusive(1, 100)
-        itemid = []
-        supware = []
-        qty = []
-        all_local = 1
-
-        for order_line in range(1, order_line_count + 1):
-            itemid.append(self.random.nurand(8191, 1, MAXITEMS))
-            if (order_line == order_line_count - 1) and (rbk == 1):
-                itemid[-1] = -1
-
-            if self.random.randint_inclusive(1, 100) != 1:
-                supware.append(w_id)
-            else:
-                supware.append(self.other_ware(w_id))
-                all_local = 0
-
-            qty.append(self.random.randint_inclusive(1, 10))
-
-        sql = 'SELECT new_order(%s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        args = (w_id, c_id, d_id, order_line_count, all_local, itemid, supware, qty, timestamp)
-        return TPCC.execute_sql(conn, sql, args)
-
-    def payment(self, conn, timestamp='NOW()'):
-        w_id = self.random.randint_inclusive(1, self.scale_factor)
-        d_id = self.random.randint_inclusive(1, DIST_PER_WARE)
-        c_id = self.random.nurand(1023, 1, CUST_PER_DIST)
-        h_amount = self.random.randint_inclusive(1, 5000)
-        c_last = self.random.lastname(self.random.nurand(255, 0, 999))
-
-        byname = self.random.randint_inclusive(1, 100) <= 60
-        if self.random.randint_inclusive(1, 100) <= 85:
-            c_w_id = w_id
-            c_d_id = d_id
-        else:
-            c_w_id = self.other_ware(w_id)
-            c_d_id = self.random.randint_inclusive(1, DIST_PER_WARE)
-
-        sql = 'SELECT payment(%s, %s, %s, %s, %s, %s, %s, %s, %s)'
-        args = (w_id, d_id, c_d_id, c_id, c_w_id, h_amount, byname, c_last, timestamp)
-        return TPCC.execute_sql(conn, sql, args)
-
-    def order_status(self, conn):
-        w_id = self.random.randint_inclusive(1, self.scale_factor)
-        d_id = self.random.randint_inclusive(1, DIST_PER_WARE)
-        c_id = self.random.nurand(1023, 1, CUST_PER_DIST)
-        c_last = self.random.lastname(self.random.nurand(255, 0, 999))
-        byname = self.random.randint_inclusive(1, 100) <= 60
-
-        sql = 'SELECT * FROM order_status(%s, %s, %s, %s, %s)'
-        args = (w_id, d_id, c_id, c_last, byname)
-        return TPCC.execute_sql(conn, sql, args)
-
-    def delivery(self, conn, timestamp='NOW()'):
-        w_id = self.random.randint_inclusive(1, self.scale_factor)
-        o_carrier_id = self.random.randint_inclusive(1, 10)
-
-        sql = 'SELECT * FROM delivery(%s, %s, %s, %s)'
-        args = (w_id, o_carrier_id, DIST_PER_WARE, timestamp)
-        return TPCC.execute_sql(conn, sql, args)
-
-    def stocklevel(self, conn):
-        w_id = self.random.randint_inclusive(1, self.scale_factor)
-        d_id = self.random.randint_inclusive(1, DIST_PER_WARE)
-        level = self.random.randint_inclusive(10, 20)
-
-        sql = 'SELECT * FROM stocklevel(%s, %s, %s)'
-        args = (w_id, d_id, level)
-        return TPCC.execute_sql(conn, sql, args)
 
 
 if __name__ == '__main__':
