f0a32a3736c8c1be5e26710562fd6dc539bd5206
Sebastian Dreßler <sebastian@swarm64.com>
2020-10-21 09:44:20 +0200

    80 cols on Q1 text

--- benchmarks/tpcc/schemas/common/1.sql
+++ benchmarks/tpcc/schemas/common/1.sql
@@ -1,12 +1,12 @@
 -- Business question:
 -- The Pricing Summary Report Query provides a summary pricing report for all
--- lineitems shipped as of a given date. The date is within 60 - 120 days of the
--- greatest ship date contained in the database. The query lists totals for
--- extended price, discounted extended price, discounted extended price plus tax,
--- average quantity, average extended price, and average discount. These aggregates
--- are grouped by RETURNFLAG and LINESTATUS, and listed in ascending order of
--- RETURNFLAG and LINESTATUS. A count of the number of lineitems in each group is
--- included.
+-- lineitems shipped as of a given date. The date is within 60 - 120 days of
+-- the greatest ship date contained in the database. The query lists totals for
+-- extended price, discounted extended price, discounted extended price plus
+-- tax, average quantity, average extended price, and average discount. These
+-- aggregates are grouped by RETURNFLAG and LINESTATUS, and listed in ascending
+-- order of RETURNFLAG and LINESTATUS. A count of the number of lineitems in
+-- each group is included.
 
 DROP FUNCTION IF EXISTS pricing_summary(DATE);
 CREATE FUNCTION pricing_summary(in_date DATE) RETURNS TABLE (
