150b8da39722299cd0e43521351eea6847e6c747
Luis Carril <luis.carril@swarm64.com>
2020-12-21 10:02:57 +0100

    Add partitioned schemas for native postgres
    Alter table commands on partitioned tables cannot be ONLY as they need to
    be propagated to the partitions themselves.
    In partitioned tables the columns covered by the primary key and unique constraints must include
    the partition key columns.
    Some foreign keys are not possible (in comparison to the psql_native
    schema), because some column-sets cannot longer be defined as unique
    without the partition key.

---
+++ benchmarks/tpch/schemas/psql_native_partitioned_date_week/foreign-keys.sql
@@ -0,0 +1,18 @@
+ALTER TABLE ONLY customer
+    ADD CONSTRAINT customer_nation_fk FOREIGN KEY (c_nationkey) REFERENCES nation(n_nationkey);
+ALTER TABLE lineitem
+    ADD CONSTRAINT lineitem_partsupp_fk FOREIGN KEY (l_partkey, l_suppkey) REFERENCES partsupp(ps_partkey, ps_suppkey);
+ALTER TABLE lineitem
+    ADD CONSTRAINT lineitem_part_fk FOREIGN KEY (l_partkey) REFERENCES part(p_partkey);
+ALTER TABLE lineitem
+    ADD CONSTRAINT lineitem_supp_fk FOREIGN KEY (l_suppkey) REFERENCES supplier(s_suppkey);
+ALTER TABLE ONLY nation
+    ADD CONSTRAINT nation_region_fk FOREIGN KEY (n_regionkey) REFERENCES region(r_regionkey);
+ALTER TABLE orders
+    ADD CONSTRAINT order_customer_fk FOREIGN KEY (o_custkey) REFERENCES customer(c_custkey);
+ALTER TABLE ONLY partsupp
+    ADD CONSTRAINT partsupp_part_fk FOREIGN KEY (ps_partkey) REFERENCES part(p_partkey);
+ALTER TABLE ONLY partsupp
+    ADD CONSTRAINT partsupp_supplier_fk FOREIGN KEY (ps_suppkey) REFERENCES supplier(s_suppkey);
+ALTER TABLE ONLY supplier
+    ADD CONSTRAINT supplier_nation_fk FOREIGN KEY (s_nationkey) REFERENCES nation(n_nationkey);
