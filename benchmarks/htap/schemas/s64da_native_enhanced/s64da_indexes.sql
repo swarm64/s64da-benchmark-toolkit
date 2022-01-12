64f9b49b4c95689129ba45bd3ccb1612dd9c9903
jase.alpers <jase.alpers@servicenow.com>
2022-01-12 13:47:12 -0600

    Updated columnstore indexes; stats are by column

--- benchmarks/htap/schemas/s64da_native_enhanced/s64da_indexes.sql
+++ benchmarks/htap/schemas/s64da_native_enhanced/s64da_indexes.sql
@@ -1,41 +1,4 @@
-CREATE INDEX idx_customer_cache ON customer USING columnstore (
-    c_id
-  , c_d_id
-  , c_w_id
-  , c_nationkey
-  , c_last
-  , c_city
-  , c_phone
-  , c_state
-  , c_balance
-);
-
-CREATE INDEX idx_orders_cache ON orders USING columnstore (
-    o_id
-  , o_d_id
-  , o_w_id
-  , o_c_id
-  , o_entry_d
-  , o_carrier_id
-  , o_ol_cnt
-  , o_all_local
-);
-
-CREATE INDEX idx_order_line_cache ON order_line USING columnstore (
-    ol_o_id
-  , ol_d_id
-  , ol_w_id
-  , ol_number
-  , ol_i_id
-  , ol_supply_w_id
-  , ol_delivery_d
-  , ol_quantity
-  , ol_amount
-);
-
-CREATE INDEX idx_stock_cache ON stock USING columnstore (
-    s_i_id
-  , s_w_id
-  , s_quantity
-  , s_order_cnt
-);
+CREATE INDEX idx_customer_cache ON customer USING columnstore ((0));
+CREATE INDEX idx_orders_cache ON orders USING columnstore ((0));
+CREATE INDEX idx_order_line_cache ON order_line USING columnstore ((0));
+CREATE INDEX idx_stock_cache ON stock USING columnstore ((0));
